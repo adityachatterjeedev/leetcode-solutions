890b3cea70e6c7892ccd80ccc006353dc40d0a4a
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-03-22 23:38:32 -0400

    File updated at 3/22/2025, 11:38:36 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.py
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.py
@@ -1,4 +1,4 @@
-# Last updated: 3/22/2025, 11:38:29 PM
+# Last updated: 3/22/2025, 11:38:36 PM
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
