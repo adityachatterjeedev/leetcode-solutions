967ddda7d9d230701d718e24cdc24cbd66d31739
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-03-24 10:46:04 -0400

    Create file

---
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.py
@@ -0,0 +1,19 @@
+# Last updated: 3/24/2025, 10:46:05 AM
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ret = []
+        for i in range(1,numRows + 1):
+            if i == 1:
+                ret.append([1])
+            elif i == 2:
+                ret.append([1,1])
+            else:
+                row = [None] * i
+                for j in range(i):
+                    if j == 0 or j == i - 1:
+                        row[j] = 1
+                    else:
+                        row[j] = ret[i - 2][j - 1] + ret[i - 2][j]
+                ret.append(row)
+        return ret
+            
