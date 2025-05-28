539ab31e15a5b00e15dc55986b30d5c1f705c43a
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-05-28 04:58:37 -0400

    Create 74-Searcha2DMatrix

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -0,0 +1,45 @@
+# Last updated: 5/28/2025, 4:58:37 AM
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        num_rows, num_cols = len(matrix), len(matrix[0])
+
+        #find the row
+
+        l, r = 0, num_rows - 1
+        target_row = -1
+        while l <= r:
+            mid = (l + r) // 2
+
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] < target:
+                if (mid == num_rows - 1) or (mid < num_rows - 1 and matrix[mid + 1][0] > target): 
+                    #found the target row
+                    target_row = mid
+                    break
+                else:
+                    l = mid + 1
+            else: #matrix[mid][0] > target
+                if (mid == 0):
+                    return False
+                else:
+                    r = mid - 1
+        if target_row == -1:
+            return False
+
+
+
+        row = matrix[target_row]
+        left, right = 0, num_cols - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            if row[middle] == target:
+                return True
+            elif row[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+        
+        return False
