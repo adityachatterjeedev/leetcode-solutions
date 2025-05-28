16b07b37bc1e3d56a78cfd08aeacd8b16130e86e
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-05-28 17:04:40 -0400

    File updated at 5/28/2025, 5:04:40 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,18 +1,11 @@
-# Last updated: 5/28/2025, 5:04:00 PM
+# Last updated: 5/28/2025, 5:04:40 PM
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        smallest_num = -1e5
-        count_of_smallest_num = 1 #invariant, always >= 1
-        write = 0
-        for curr_pos,curr_num in enumerate(nums):
-            if curr_num > smallest_num:
-                smallest_num = curr_num
-                count_of_smallest_num = 1
-            else:
-                count_of_smallest_num += 1
-            
-            if count_of_smallest_num < 3:
-                nums[write] = smallest_num
-                write += 1
+        k=2
+        for i in range(2,len(nums)):
+            if nums[k-2]!=nums[i]:
+                nums[k]=nums[i]
+                k+=1
+                i+=1
+        return k
         
-        return write
