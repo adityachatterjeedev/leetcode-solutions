d2c84d581846e4b00803b315585f48d4e41c6dd9
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-05-29 22:32:30 -0400

    File updated at 5/29/2025, 10:32:32 PM

--- 189-RotateArray/189-RotateArray.py
+++ 189-RotateArray/189-RotateArray.py
@@ -1,22 +1,15 @@
-# Last updated: 5/21/2025, 6:33:02 PM
+# Last updated: 5/29/2025, 10:32:32 PM
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        arr_len = len(nums)
-        k = k % arr_len
+        k = k % len(nums)
 
-        def reverse(i, j):
-            l, r = i, j
-            while l < r:
-                nums[l], nums[r] = nums[r], nums[l]
-                l, r = l + 1, r - 1
+        nums.reverse()
 
-        # reverse the whole array
-        reverse(0, arr_len - 1)
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
 
-        #reverse first k
-        reverse(0, k - 1)
 
-        reverse(k, arr_len - 1)
+        
