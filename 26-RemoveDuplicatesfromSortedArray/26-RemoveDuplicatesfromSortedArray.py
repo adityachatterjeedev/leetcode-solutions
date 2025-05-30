e4f16b21be4d91007d117cdc4094a9aea83242ee
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-05-29 22:25:32 -0400

    File updated at 5/29/2025, 10:25:34 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,11 +1,11 @@
-# Last updated: 5/29/2025, 10:22:55 PM
+# Last updated: 5/29/2025, 10:25:34 PM
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        unique_num_count = 0
+        k = 0
         max_number_so_far = -500
-        for i in range(len(nums)):
-            if nums[i] > max_number_so_far:
-                max_number_so_far = nums[i]
-                nums[unique_num_count] = nums[i]
-                unique_num_count += 1
-        return unique_num_count
+        for num in nums:
+            if num > max_number_so_far:
+                max_number_so_far = num
+                nums[k] = num
+                k += 1
+        return k
