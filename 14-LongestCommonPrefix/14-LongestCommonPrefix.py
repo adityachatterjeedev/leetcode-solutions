47e00e212376a870358c308347f6ebd7e662d1d0
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-05-31 19:01:28 -0400

    File updated at 5/31/2025, 7:01:28 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,22 +1,10 @@
-# Last updated: 3/22/2025, 11:01:27 PM
-def compareTwoStrings(string1: str, string2: str) -> str:
-    if not string1 or not string2:
-        return ''
-    length = min(len(string1), len(string2))
-    for i in range (length):
-        if string1[i] != string2[i]:
-            if i == 0:
-                return ''
-            else:
-                return string1[:i]
-        elif i == length - 1:
-            return string1[:i+1]
-
-
+# Last updated: 5/31/2025, 7:01:28 PM
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix = strs[0]
-        for i in range(1,len(strs)):
-            prefix = compareTwoStrings(prefix, strs[i])
-            if prefix == '': return prefix
-        return prefix
+        pref = strs[0]
+        for i in range(1, len(strs)):
+            while not strs[i].startswith(pref):
+                pref = pref[:-1]
+                if pref == '':
+                    return ''
+        return pref
