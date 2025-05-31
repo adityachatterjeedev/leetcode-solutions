255d2cc573a93d1aadde6d1752928183161513d8
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-05-31 19:02:02 -0400

    File updated at 5/31/2025, 7:02:02 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,10 +1,22 @@
-# Last updated: 5/31/2025, 7:01:28 PM
+# Last updated: 5/31/2025, 7:02:02 PM
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pref = strs[0]
-        for i in range(1, len(strs)):
-            while not strs[i].startswith(pref):
-                pref = pref[:-1]
-                if pref == '':
-                    return ''
-        return pref
+
+        if not strs:
+            return ""
+
+        ans=strs[0]
+
+        for i in strs:
+            while not i.startswith(ans): 
+                ans= ans[:-1]
+                if not ans:
+                    return ""
+
+        return ans 
+
+
+
+
+
+        
