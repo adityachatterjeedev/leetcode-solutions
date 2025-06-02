bafe49ca1d02a0862cb364d5cfd98204e028313b
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-06-02 01:48:08 -0400

    File updated at 6/2/2025, 1:48:09 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,4 +1,4 @@
-# Last updated: 6/2/2025, 1:47:50 AM
+# Last updated: 6/2/2025, 1:48:09 AM
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left,right = 0, len(numbers) - 1
