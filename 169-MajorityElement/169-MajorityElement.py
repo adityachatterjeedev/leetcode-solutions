bb9f7dd6c1e4fce7dd2179371c4bc3873ecc7dbd
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-06-14 04:37:28 -0400

    File updated at 6/14/2025, 4:37:28 AM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,15 +1,18 @@
-# Last updated: 6/4/2025, 5:53:28 PM
+# Last updated: 6/14/2025, 4:37:28 AM
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        #Moore's Voting Algorithm
+        candidate = None
+        count = 0
 
-        el, count = None, 0
         for num in nums:
             if count == 0:
-                el = num
+                candidate = num
                 count = 1
-            elif num == el:
-                count += 1
-            else:
+            elif num != candidate:
                 count -= 1
+            else:
+                count += 1
+        
+        return candidate
 
-        return el
