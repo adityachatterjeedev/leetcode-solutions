b07a00374812bd4fcf00dcfabd7593283432e936
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-06-21 15:24:54 -0400

    File updated at 6/21/2025, 3:24:55 PM

--- 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.py
+++ 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.py
@@ -1,22 +1,14 @@
-# Last updated: 6/21/2025, 3:19:23 PM
+# Last updated: 6/21/2025, 3:24:55 PM
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        odd_count = 0
-        counts = {0: 1}
-
-        result = 0
-
-        for num in nums:
-            odd_count += (0 if (num % 2 == 0) else 1)
-            diff = odd_count - k
-            if diff in counts:
-                result += counts[diff]
-            
-            if odd_count in counts:
-                counts[odd_count] += 1
-            else:
-                counts[odd_count] = 1
-
-        return result
-
-        
+        oddidx=[-1]
+        ans=0
+        for i in range(len(nums)):
+            if nums[i]%2:
+                oddidx.append(i)
+        oddidx.append(len(nums))
+        for i in range(1,len(oddidx)-k):
+            leftside=oddidx[i]-oddidx[i-1]
+            rightside=oddidx[i+k]-oddidx[i+k-1]
+            ans+=leftside*rightside
+        return ans
