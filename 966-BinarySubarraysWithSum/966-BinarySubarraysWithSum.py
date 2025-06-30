349c4896773786ff8adbf0713281c1f29adb5ef2
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-06-29 20:58:16 -0400

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.py
@@ -0,0 +1,19 @@
+# Last updated: 6/29/2025, 8:58:16 PM
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        num_sum, result = 0, 0
+        pref_counts = {0 : 1}
+
+        for num in nums:
+            num_sum += num
+            if num_sum >= goal:
+                diff = num_sum - goal
+                if diff in pref_counts:
+                    result += pref_counts[diff]
+
+            if num_sum in pref_counts:
+                pref_counts[num_sum] += 1
+            else:
+                pref_counts[num_sum] = 1
+
+        return result
