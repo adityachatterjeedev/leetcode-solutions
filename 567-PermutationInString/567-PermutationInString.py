c7d93bbface663c0b0c84b9d4ae6c817ce83e9d2
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-01 02:44:56 -0400

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.py
@@ -0,0 +1,36 @@
+# Last updated: 7/1/2025, 2:44:57 AM
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l1, l2 = len(s1), len(s2)
+        if l1 > l2:
+            return False
+        
+        counts1 = {}
+        for letter in s1:
+            counts1[letter] = 1 + counts1.get(letter, 0)
+
+        left, right = 0, l1 - 1
+        counts2 = {}
+        for i in range(l1):
+            letter = s2[i]
+            counts2[letter] = 1 + counts2.get(letter, 0)
+
+        while right < l2 - 1:
+            if counts2 == counts1:
+                return True
+            #remove left
+            left_letter = s2[left]
+            counts2[left_letter] -= 1
+            if counts2[left_letter] == 0:
+                del(counts2[left_letter])
+            left += 1
+
+            #add right
+            right += 1
+            right_letter = s2[right]
+            counts2[right_letter] = 1 + counts2.get(right_letter, 0)
+
+        if counts2 == counts1:
+            return True
+
+        return False
