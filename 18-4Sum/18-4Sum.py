dd419674d6b7519dc4fcad855614e3670acb96be
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-19 20:45:26 -0400

    File updated at 7/19/2025, 8:45:26 PM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,36 +1,53 @@
-# Last updated: 7/19/2025, 8:34:29 PM
+# Last updated: 7/19/2025, 8:45:26 PM
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        e1 = 0
-        length = len(nums)
-        result = []
-        while e1 < length - 3:
-            e2 = e1 + 1
-            while e2 < length - 2:
-                goal = target - (nums[e1] + nums[e2])
-                left, right = e2 + 1, length - 1
-                while left < right:
-                    total = nums[left] + nums[right]
-                    if total == goal:
-                        result.append([nums[e1], nums[e2], nums[left], nums[right]])
-                        n = left + 1
-                        while n < right and nums[n] == nums[n - 1]:
-                            n += 1
-                        left = n
-                    elif total < goal:
-                        left += 1
-                    else:
-                        right -= 1
+        n = len(nums)
+        ans = []
 
-                nxt = e2 + 1
-                while nxt < length - 2 and nums[nxt] == nums[nxt - 1]:
-                    nxt += 1
-                e2 = nxt
+        for a in range(n - 3):
+            x = nums[a]
+
+            if a and nums[a] == nums[a - 1]:
+                continue
             
-            nextindex = e1 + 1
-            while nextindex < length - 3 and nums[nextindex] == nums[nextindex - 1]:
-                nextindex += 1
-            e1 = nextindex
+            if x + nums[a + 1] + nums[a + 2] + nums[a + 3] > target:
+                break
 
-        return result
+            if x + nums[-1] + nums[-2] + nums[-3] < target:
+                continue
+
+            for b in range(a + 1, n - 2):
+                y = nums[b]
+
+                if b > a + 1 and nums[b] == nums[b - 1]:
+                    continue
+                
+                if x + y + nums[b + 1] + nums[b + 2] > target:
+                    break
+                
+                if x + y + nums[-1] + nums[-2] < target:
+                    continue
+
+                c = b + 1
+                d = n - 1
+                while c < d:
+                    s = x + y + nums[c] + nums[d]
+
+                    if s < target:
+                        c += 1
+                    elif s > target:
+                        d -= 1
+                    else:
+                        ans.append([x, y, nums[c], nums[d]])
+
+                        c += 1
+                        while c < d and nums[c] == nums[c - 1]:
+                            c += 1
+                        
+                        d -= 1
+                        while c < d and nums[d] == nums[d + 1]:
+                            d -= 1
+        
+        return ans
+            
