563cc43d10cea8fdb59674af2fc1f29bcdf6eb56
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-22 19:25:11 -0400

    File updated at 7/22/2025, 7:25:12 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,4 +1,4 @@
-# Last updated: 7/22/2025, 7:24:05 PM
+# Last updated: 7/22/2025, 7:25:12 PM
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefs = {0: 1}
@@ -6,8 +6,9 @@ class Solution:
         count = 0
         for num in nums:
             curr_sum += num
-            if curr_sum - k in prefs:
-                count += prefs[curr_sum - k]
+            diff = curr_sum - k
+            if diff in prefs:
+                count += prefs[diff]
             prefs[curr_sum] = 1 + prefs.get(curr_sum, 0)
 
         return count
