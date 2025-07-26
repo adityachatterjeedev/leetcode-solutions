fd4244593ddb1e8c0aea19b47b4f29481e218621
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-26 01:41:11 -0400

    Create 304-RangeSumQuery2D-Immutable

---
+++ 304-RangeSumQuery2D-Immutable/version/python/304-RangeSumQuery2D-Immutable_20250726_014112.py
@@ -0,0 +1,16 @@
+# Last updated: 7/26/2025, 1:41:12 AM
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        ROWS, COLS = len(matrix), len(matrix[0])
+        self.sumMat = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
+
+        for row in range(ROWS):
+            prefix = 0
+            for col in range(COLS):
+                prefix += matrix[row][col]
+                self.sumMat[row + 1][col + 1] = prefix + self.sumMat[row][col + 1]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        row1, col1 ,row2, col2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
+        return self.sumMat[row2][col2] - self.sumMat[row1 - 1][col2] - self.sumMat[row2][col1 - 1] + self.sumMat[row1 - 1][col1 - 1]
