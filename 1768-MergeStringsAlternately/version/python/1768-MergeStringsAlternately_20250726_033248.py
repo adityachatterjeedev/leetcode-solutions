06208ad5fce7448f90b0b41f06af1c843f2c80ed
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-26 03:32:47 -0400

    Create 1768-MergeStringsAlternately

---
+++ 1768-MergeStringsAlternately/version/python/1768-MergeStringsAlternately_20250726_033248.py
@@ -0,0 +1,23 @@
+# Last updated: 7/26/2025, 3:32:48 AM
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        n1, n2 = len(word1), len(word2)
+        c1, c2 = 0,0
+        flag = True
+        result = ''
+        while c1 + c2 < n1 + n2:
+            if c1 == n1:
+                result += word2[c2]
+                c2 += 1
+            elif c2 == n2:
+                result += word1[c1]
+                c1 += 1
+            elif flag:
+                result += word1[c1]
+                c1 += 1
+                flag = not flag
+            else:
+                result += word2[c2]
+                c2 += 1
+                flag = not flag
+        return result
