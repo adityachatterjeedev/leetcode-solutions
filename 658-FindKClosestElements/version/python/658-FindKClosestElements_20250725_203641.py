12e9758479c1f361b505d1fea3e7ae19fef93bd3
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-25 20:36:40 -0400

    Create 658-FindKClosestElements

---
+++ 658-FindKClosestElements/version/python/658-FindKClosestElements_20250725_203641.py
@@ -0,0 +1,28 @@
+# Last updated: 7/25/2025, 8:36:41 PM
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if x <= arr[0]:
+            return arr[:k]
+        if x >= arr[-1]:
+            return arr[-k:]
+        
+        l, r = 0, len(arr) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if arr[mid] < x:
+                l = mid + 1
+            else:
+                r = mid
+
+        l, r = l - 1, l
+        while r - l - 1 < k:
+            if l < 0:
+                r += 1
+            elif r >= len(arr):
+                l -= 1
+            elif abs(arr[l] - x) <= abs(arr[r] - x):
+                l -= 1
+            else:
+                r += 1
+
+        return arr[l + 1:r]
