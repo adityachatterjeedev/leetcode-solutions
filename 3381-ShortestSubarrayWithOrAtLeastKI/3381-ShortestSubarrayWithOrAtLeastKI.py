ed93a7bd6df7a2c3858e7ffb29b2257636fe460a
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-07-25 20:31:15 -0400

    Create 3381-ShortestSubarrayWithOrAtLeastKI

---
+++ 3381-ShortestSubarrayWithOrAtLeastKI/3381-ShortestSubarrayWithOrAtLeastKI.py
@@ -0,0 +1,20 @@
+# Last updated: 7/25/2025, 8:31:16 PM
+class Solution:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+
+        n = len(nums)
+        res = float('inf')
+        left = 0
+        curr_or = 0
+
+        for right in range(n):
+            curr_or |= nums[right]
+            while left <= right and curr_or >= k:
+                res = min(res, right - left + 1)
+
+                left += 1
+                curr_or = 0
+                for i in range(left, right + 1):
+                    curr_or |= nums[i]
+
+        return res if res != float('inf') else -1
