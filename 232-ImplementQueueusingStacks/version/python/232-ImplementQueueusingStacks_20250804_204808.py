54880c92792efb8f336765da7b49d17756ebb1c5
Aditya Chatterjee <chatterjee2307@gmail.com>
2025-08-04 20:48:08 -0400

    Create 232-ImplementQueueusingStacks

---
+++ 232-ImplementQueueusingStacks/version/python/232-ImplementQueueusingStacks_20250804_204808.py
@@ -0,0 +1,36 @@
+# Last updated: 8/4/2025, 8:48:08 PM
+class MyQueue:
+
+    def __init__(self):
+
+        self.pushstack = []
+        self.popstack = []
+
+
+    def push(self, x: int) -> None:
+        self.pushstack.append(x)
+
+    def pop(self) -> int:
+        if self.popstack:
+            return self.popstack.pop()
+        else:
+            for _ in range(len(self.pushstack) - 1):
+                self.popstack.append(self.pushstack.pop())
+            return self.pushstack.pop()
+
+    def peek(self) -> int:
+        if self.popstack:
+            return self.popstack[-1]
+        else:
+            return self.pushstack[0]
+
+    def empty(self) -> bool:
+        return (len(self.pushstack) + len(self.popstack)) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
