bfe0f86ad0aa474ac01a911155c9e1d3f7a8c85e
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-08 03:49:19 -0500

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.py
@@ -0,0 +1,14 @@
+# Last updated: 2/8/2026, 3:49:20 AM
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for op in operations:
+            if op == "+":
+                stack.append(stack[-1] + stack[-2])
+            elif op == "D":
+                stack.append(2 * stack[-1])
+            elif op == "C":
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
