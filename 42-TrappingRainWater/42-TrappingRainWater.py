fcce577d007b6be275c39051c714d612e8f814e3
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-08 03:50:21 -0500

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -0,0 +1,26 @@
+# Last updated: 2/8/2026, 3:50:21 AM
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        prefs = [0] * n
+        suffs = [0] * n
+
+        prefs[0] = height[0]
+        maxPref = height[0]
+        for i in range(1, n):
+            if height[i] > maxPref:
+                maxPref = height[i]
+            prefs[i] = maxPref
+
+        suffs[n - 1] = height[n - 1]
+        maxSuff = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            if height[i] > maxSuff:
+                maxSuff = height[i]
+            suffs[i] = maxSuff
+
+        result = 0
+        for i in range(n):
+            result += min(prefs[i], suffs[i]) - height[i]
+
+        return result
