44908fa60e5030b70d41be8386901fe6fe0b43de
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-08 03:51:45 -0500

    File updated at 2/8/2026, 3:51:46 AM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,25 +1,27 @@
-# Last updated: 6/1/2025, 2:36:20 AM
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        result = []
-        for i, num in enumerate(nums):
-            if num > 0:
-                break
-            if i == 0 or (nums[i - 1] != nums[i]):
-                target = -num
-                l, r = i + 1, len(nums) - 1
-                while l < r:
-                    total = nums[l] + nums[r]
-                    if total < target:
-                        l += 1
-                    elif total > target:
-                        r -= 1
-                    else:
-                        left = nums[l]
-                        result += [[num, left, nums[r]]]
-                        while nums[l] == left and l < r:
-                            l += 1
-        
-        return result
-                        
+# Last updated: 2/8/2026, 3:51:46 AM
+1class Solution:
+2    def threeSum(self, nums: List[int]) -> List[List[int]]:        
+3        nums.sort()
+4        res = []
+5
+6        for i, num in enumerate(nums):
+7            if num > 0:
+8                break
+9            if i == 0 or num != nums[i - 1]:
+10                left = i + 1                    
+11                right = len(nums) - 1
+12
+13                while left < right:
+14                    target = -num
+15                    total = nums[left] + nums[right]
+16                    if total == target:
+17                        l_num = nums[left]
+18                        res.append([num, l_num, nums[right]])
+19                        while nums[left] == l_num and left < right:
+20                            left += 1
+21                    elif total < target:
+22                        left += 1
+23                    else:
+24                        right -= 1
+25                        
+26        return res
