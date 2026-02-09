8b950f1cdb3a458ba653dd14d532b57bc6831d02
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-08 22:00:17 -0500

    File updated at 2/8/2026, 10:00:17 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,27 +1,32 @@
-# Last updated: 2/8/2026, 3:52:26 AM
+# Last updated: 2/8/2026, 10:00:17 PM
 1class Solution:
 2    def threeSum(self, nums: List[int]) -> List[List[int]]:        
 3        nums.sort()
 4        res = []
 5
-6        for i, num in enumerate(nums):
-7            if num > 0:
+6        for i in range(len(nums) - 2):
+7            if nums[i] > 0:
 8                break
-9            if i == 0 or num != nums[i - 1]:
-10                left = i + 1                    
-11                right = len(nums) - 1
-12
-13                while left < right:
-14                    target = -num
-15                    total = nums[left] + nums[right]
-16                    if total == target:
-17                        l_num = nums[left]
-18                        res += [[num, l_num, nums[right]]]
-19                        while nums[left] == l_num and left < right:
-20                            left += 1
-21                    elif total < target:
-22                        left += 1
-23                    else:
-24                        right -= 1
-25                        
-26        return res
+9            num = nums[i]
+10            if num + nums[i] + nums[i + 1] > 0:
+11                break
+12            if num + nums[-1] + nums[-2] <0:
+13                continue
+14            if i == 0 or num != nums[i - 1]:
+15                left = i + 1                    
+16                right = len(nums) - 1
+17
+18                while left < right:
+19                    target = -num
+20                    total = nums[left] + nums[right]
+21                    if total == target:
+22                        l_num = nums[left]
+23                        res += [[num, l_num, nums[right]]]
+24                        while nums[left] == l_num and left < right:
+25                            left += 1
+26                    elif total < target:
+27                        left += 1
+28                    else:
+29                        right -= 1
+30                        
+31        return res
