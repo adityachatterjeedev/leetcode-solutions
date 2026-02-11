b1a3472a693b3db79f4c79953a4d8b844ff201b4
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-11 04:55:16 -0500

    File updated at 2/11/2026, 4:55:17 AM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,27 +1,30 @@
-# Last updated: 6/2/2025, 2:38:35 AM
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        #sliding window
-        length = len(s)
-        if length in [0,1]:
-            return length
-
-        l, r = 0,0
-        maxlen = 0
-        contents = {s[0]}
-        while r < length - 1:
-            r += 1
-            if s[r] not in contents:
-                contents.add(s[r])
-                if r == length - 1:
-                    maxlen = max(maxlen, len(contents))
-            else:
-                maxlen = max(maxlen, len(contents))
-                letter = s[r]
-                while s[l] != letter:
-                    contents.remove(s[l])
-                    l += 1
-                #do this again to remove the dupe from the substring
-                l += 1
-
-        return maxlen
+# Last updated: 2/11/2026, 4:55:17 AM
+1class Solution:
+2    def lengthOfLongestSubstring(self, s: str) -> int:
+3        if not s:
+4            return 0
+5        length = len(s)
+6        max_len = 1
+7        contents = set(s[0])
+8        left, right = 0, 0
+9        curr = 1
+10
+11        while right < length - 1:
+12            nxt = s[right + 1]
+13            if nxt not in contents:
+14                right += 1
+15                curr += 1
+16                contents.add(nxt)
+17                if right == length - 1:
+18                    return curr if curr > max_len else max_len
+19            else:
+20                max_len = curr if curr > max_len else max_len
+21                while s[left] != nxt:
+22                    contents.remove(s[left])
+23                    left += 1
+24                left += 1
+25                right += 1
+26                curr = right - left + 1
+27
+28        return max_len
+29
