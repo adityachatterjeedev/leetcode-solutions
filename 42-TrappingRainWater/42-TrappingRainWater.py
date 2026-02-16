64a76f1a6518e106c596f004626f4d407a6d1d69
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-16 05:30:21 -0500

    File updated at 2/16/2026, 5:30:21 AM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,26 +1,21 @@
-# Last updated: 2/8/2026, 3:50:21 AM
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        n = len(height)
-        prefs = [0] * n
-        suffs = [0] * n
-
-        prefs[0] = height[0]
-        maxPref = height[0]
-        for i in range(1, n):
-            if height[i] > maxPref:
-                maxPref = height[i]
-            prefs[i] = maxPref
-
-        suffs[n - 1] = height[n - 1]
-        maxSuff = height[n - 1]
-        for i in range(n - 2, -1, -1):
-            if height[i] > maxSuff:
-                maxSuff = height[i]
-            suffs[i] = maxSuff
-
-        result = 0
-        for i in range(n):
-            result += min(prefs[i], suffs[i]) - height[i]
-
-        return result
+# Last updated: 2/16/2026, 5:30:21 AM
+1class Solution:
+2    def trap(self, height: List[int]) -> int:
+3        if not height:
+4            return 0
+5
+6        left ,right = 0, len(height) - 1
+7        res = 0
+8        l_max, r_max = height[0], height[-1]
+9
+10        while left < right:
+11            if l_max < r_max:
+12                left += 1
+13                l_max = max(l_max, height[left])
+14                res += l_max - height[left]
+15            else:
+16                right -= 1
+17                r_max = max(r_max, height[right])
+18                res += r_max - height[right]
+19        
+20        return res
