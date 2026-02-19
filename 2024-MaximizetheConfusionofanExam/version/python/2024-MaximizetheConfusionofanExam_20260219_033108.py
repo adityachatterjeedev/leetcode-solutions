44824a291f87911f997747370377d7ae522fa59a
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-02-19 03:31:07 -0500

    Create 2024-MaximizetheConfusionofanExam

---
+++ 2024-MaximizetheConfusionofanExam/version/python/2024-MaximizetheConfusionofanExam_20260219_033108.py
@@ -0,0 +1,25 @@
+# Last updated: 2/19/2026, 3:31:08 AM
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        ans = 0
+        n = len(answerKey)
+        cntt = 0
+        cntf = 0
+        l = 0
+
+        for r in range(n):
+            if answerKey[r] == "T":
+                cntt += 1
+            else:
+                cntf += 1
+
+            while min(cntt, cntf) > k:
+                if answerKey[l] == "T":
+                    cntt -= 1
+                else:
+                    cntf -= 1
+                l += 1
+
+            ans = max(ans, r - l + 1)
+
+        return ans
