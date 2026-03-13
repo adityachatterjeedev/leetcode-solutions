7d4cea73db12f01a1f507a941e16f2dc78ec24c5
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-03-13 17:36:05 -0400

    Create 1011-CapacityToShipPackagesWithinDDays

---
+++ 1011-CapacityToShipPackagesWithinDDays/version/python/1011-CapacityToShipPackagesWithinDDays_20260313_173606.py
@@ -0,0 +1,26 @@
+# Last updated: 3/13/2026, 5:36:06 PM
+1class Solution:
+2    def shipWithinDays(self, weights: List[int], days: int) -> int:
+3
+4        l, r = max(weights), sum(weights)
+5
+6        candidate = r
+7
+8        while l <= r:
+9            mid = (l + r) // 2
+10            total_days = 0
+11            cum_weights = 0
+12            for weight in weights:
+13                cum_weights += weight
+14                if cum_weights > mid:
+15                    total_days += 1
+16                    cum_weights = weight
+17            total_days += 1
+18
+19            if total_days > days:
+20                l = mid + 1
+21            else:
+22                candidate = mid
+23                r = mid - 1
+24
+25        return candidate
