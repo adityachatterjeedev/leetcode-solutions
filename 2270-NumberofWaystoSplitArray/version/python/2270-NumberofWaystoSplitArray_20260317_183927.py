778f9e8f815d31da8ffcc5c194fc3c0bc4830b97
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-03-17 18:39:26 -0400

    Create 2270-NumberofWaystoSplitArray

---
+++ 2270-NumberofWaystoSplitArray/version/python/2270-NumberofWaystoSplitArray_20260317_183927.py
@@ -0,0 +1,13 @@
+# Last updated: 3/17/2026, 6:39:27 PM
+1class Solution:
+2    def waysToSplitArray(self, nums: List[int]) -> int:
+3
+4        total = sum(nums)
+5        count = 0
+6        left = 0
+7        for i in range(len(nums) - 1):
+8            left += nums[i]
+9            if left >= total - left:
+10                count += 1
+11
+12        return count
