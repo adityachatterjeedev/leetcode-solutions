9737ada06335b92426014807a08719147f63a4af
Aditya Chatterjee <chatterjee2307@gmail.com>
2026-07-07 18:36:06 -0400

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.py
@@ -0,0 +1,18 @@
+# Last updated: 7/7/2026, 6:36:05 PM
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        total = 1
+        while self.stack and self.stack[-1][0] <= price:
+            total += self.stack.pop()[1]
+        self.stack.append((price, total))
+        return total
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
